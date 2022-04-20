3e20be3cd92794426e4f6250335003dc59142307
Carlos Valenzuela B <cvalenzuelab1982@gmail.com>
2022-04-19 20:10:43 -0500

    Agregar archivos de proyecto.

---
+++ UsoGitHubApp/Controllers/WeatherForecastController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace UsoGitHubApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hola mundo");
+        }
+    }
+}
